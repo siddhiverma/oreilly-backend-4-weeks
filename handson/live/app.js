92a1f3a56e43b79b445582945ce0eb232bc7c982
Sid <siddhi.sverma@gmail.com>
2023-04-11 15:39:19 +0530

    using child process

--- handson/live/app.js
+++ handson/live/app.js
@@ -1,10 +1,18 @@
+const {fork} = require('child_process');
+const fib = fork('fibonacci.js');
+
+
 console.log('hello world');
 
-function fibonacci(num) {
-    if (num <= 1) return 1;
-    return fibonacci(num-1)+fibonacci(num-2);
-}
+// function fibonacci(num) {
+//     if (num <= 1) return 1;
+//     return fibonacci(num-1)+fibonacci(num-2);
+// }
 
-console.log(fibonacci(44));
+fib.send(44);
+
+// console.log(fibonacci(44));
+
+fib.on('message', (fibonacciValue) => console.log(fibonacciValue));
 
 console.log('program ended')
