ff0a1544e9a3c5a6ac75c266ac14b835f3687d44
Sid <siddhi.sverma@gmail.com>
2023-04-11 15:24:14 +0530

    fibonacci

--- handson/live/app.js
+++ handson/live/app.js
@@ -1,1 +1,10 @@
 console.log('hello world');
+
+function fibonacci(num) {
+    if (num <= 1) return 1;
+    return fibonacci(num-1)+fibonacci(num-2);
+}
+
+console.log(fibonacci(44));
+
+console.log('program ended')
